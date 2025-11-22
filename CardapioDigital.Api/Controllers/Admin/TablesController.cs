b66fdf39bbc29be6766032b248f0e8becefde320
Gustavo <Gustavo@WIN-216HVE2K6U1>
2025-11-22 18:05:50 -0300

    TablesController

---
+++ CardapioDigital.Api/Controllers/Admin/TablesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CardapioDigital.Api.Controllers.Admin
+{
+    [ApiController]
+    [Route("admin/restaurants/{rid:guid}/tables")]
+    [Authorize]
+    public class TablesController:ControllerBase
+    {
+    }
+}
