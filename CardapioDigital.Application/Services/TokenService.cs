9ab6a83323f5895e8ce84de70f9661f0ea88ac89
Gustavo <Gustavo@WIN-216HVE2K6U1>
2025-11-22 17:51:32 -0300

    TokenService and updating Migration with new propertie on Table

---
+++ CardapioDigital.Application/Services/TokenService.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardapioDigital.Application.Services
+{
+    public class TokenService
+    {
+        private readonly string _salt;
+        public TokenService(IConfiguration cfg) => _salt = cfg["Token:Salt"];
+
+        public string GenerateToken(int length = 10)
+        {   
+            const string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+            var random = new byte[length];
+            using var rng = RandomNumberGenerator.Create();
+            rng.GetBytes(random);
+
+            var result = new StringBuilder(length);
+
+            for (int i = 0; i < length; i++)
+            {
+                result.Append(chars[random[i] % chars.Length]);
+            }
+
+            return result.ToString();
+        }
+        public string HashToken(string token)
+        {
+            using var sha = SHA256.Create();
+            var bytes = Encoding.UTF8.GetBytes(token + _salt);
+            return Convert.ToHexString(sha.ComputeHash(bytes));
+        }
+
+        public bool VerifyToken(string token, string storedHash)
+            => HashToken(token) == storedHash;
+    }
+
+}
---
+++ CardapioDigital.Infrastructure/Migrations/20251122203707_AddTableTokenFields.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CardapioDigital.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTableTokenFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "TokenExpiresAt",
+                table: "Tables",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TokenExpiresAt",
+                table: "Tables");
+        }
+    }
+}
